2d511fc445024f14405d34c9ed5eaadcb3ae0eed
neuli1980 <56549849+neuli1980@users.noreply.github.com>
2019-12-22 14:09:59 +0100

    Update Program.cs

--- src/Test/ChoCSVReaderTest/Program.cs
+++ src/Test/ChoCSVReaderTest/Program.cs
@@ -716,11 +716,11 @@ somethingdownhere,thisisthelastuser,andthisisthelastpassword
         [Test]
         public static void MergeCSV1()
         {
-            string expected = @"Id	Name	City\r\n" +
+            string expected = $"Id	Name	City\r\n" +
 $"1	Tom	Las Vegas\r\n" +
 $"2	Mark	Dallas";
 
-expected = $"Id	Name	City
+expected = @"Id	Name	City
 1	Tom	Las Vegas
 2	Mark	Dallas";
 
